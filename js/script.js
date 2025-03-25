b1cfb2b9fffeb251a39dc4592b6c77e41ae54c8e
orion vale <www.orionvale29@gmail.com>
2025-03-24 22:51:55 -0700

    Initial commit: Add portfolio prototye

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Welcome to Orion's Portfolio!");
+    // You can add additional interactivity here later
+  });
+  
